a4491e8dde504f5a2fdca93dcd21e0ba28590bc6
Francesc Pellicero <pelly.obn91@gmail.com>
2021-02-21 12:22:39 +0100

    Updates

--- _next/static/1jvLEN0wHEh7Zb9Azfsuj/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fportfolio","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fblog"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
